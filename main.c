c34b97825e8082ba08fba8c5d6ef98ae39165564
Simone Margio <simone.margio@icloud.com>
2020-05-03 19:29:07 +0200

    Add: info sul progetto in main

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "Biblioteca.h"
 
+/*
+ * Laboratorio di algoritmi e strutture dati
+ * Studente: Simone Margio N86001098
+ * Progetto: gestione biblioteca
+ *
+ */
 
 int main() {
     F_gestione_biblioteca();
